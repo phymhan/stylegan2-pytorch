475f48f230f95c544173b9ee24db2bc3eb7ce8cd
Ligong Han <hanligong@gmail.com>
2021-01-15 23:40:24 -0500

    process vox2, remove nested directories

--- dataset.py
+++ dataset.py
@@ -3,6 +3,22 @@ from io import BytesIO
 import lmdb
 from PIL import Image
 from torch.utils.data import Dataset
+import pickle
+import os
+import numpy as np
+
+
+def is_image_file(filename):
+    """Checks if a file is an image.
+
+    Args:
+        filename (string): path to a file
+
+    Returns:
+        bool: True if the filename ends with a known image extension
+    """
+    filename_lower = filename.lower()
+    return any(filename_lower.endswith(ext) for ext in IMG_EXTENSIONS)
 
 
 class MultiResolutionDataset(Dataset):
@@ -38,3 +54,94 @@ class MultiResolutionDataset(Dataset):
         img = self.transform(img)
 
         return img
+
+
+class VideoFolderDataset(Dataset):
+    def __init__(self, dataroot, transform=None, mode='video', min_len=8, cache=None):
+        self.root = dataroot
+        self.transform = transform or lambda x: x
+        self.cache = cache
+        self.mode = mode
+        self.min_len = min_len
+        self.videos = []
+        self.lengths = []
+        if cache is not None and os.path.exists(cache):
+            with open(cache, 'rb') as f:
+                self.videos, self.lengths = pickle.load(f)
+        else:
+            video_list = []
+            length_list = []
+            for i, video in enumerate(os.listdir(dataroot)):
+                try:
+                    frames = sorted(os.listdir(os.path.join(dataroot, video)))
+                except ValueError:
+                    continue
+                frame_list = []
+                for j, frame_name in enumerate(frames):
+                    if is_image_file(os.path.join(dataroot, video, frame_name)):
+                        # do not include dataroot here so that cache can be shared
+                        frame_list.append(os.path.join(video, frame_name))
+                if len(frame_list) >= min_len:
+                    video_list.append(frame_list)
+                    length_list.append(len(frame_list))
+            self.videos, self.lengths = video_list, length_list
+            if cache is not None:
+                with open(cache, 'wb') as f:
+                    pickle.dump((self.videos, self.lengths), f)
+        self.cumsum = np.cumsum([0] + self.lengths)
+        print("Total numver of videos {}.".format(len(self.videos)))
+        print("Total number of frames {}.".format(np.sum(self.lengths)))
+
+    def _get_video(self, index, n_frames):
+        # copied from Yu
+        video = self.videos[index]
+        video_len = self.lengths[index]
+        
+        start_idx = random.randint(0, video_len-1-n_frames*FRAME_STEP)
+        img = Image.open(video[0])
+        h, w = img.height, img.width
+        
+        NEED_CROP = False
+        if h > w:
+            NEED_CROP = True
+            half = (h-w) // 2
+            cropsize = (0, half, w, half+w) # left, upper, right, lower
+        elif w > h:
+            NEED_CROP = True
+            half = (w-h) // 2
+            cropsize = (half, 0, half+h, h)
+
+        images = []
+        
+        for i in range(start_idx, start_idx+n_frames*FRAME_STEP, FRAME_STEP):
+            path = video[i]
+            img = Image.open(path)
+            if NEED_CROP:
+                img = img.crop(cropsize)
+            if img.height != img.width:
+                print('crop error!')
+                abc = 1
+            img = img.resize(self.opt.fineSize, Image.ANTIALIAS)
+            img = np.asarray(img, dtype=np.float32)
+            img /= 255.
+            img_tensor = preprocess(img).unsqueeze(0)
+            images.append(img_tensor)
+        
+        video_clip = torch.cat(images)
+        return video_clip
+    
+    def _get_image(self, index):
+        # copied from MoCoGAN
+        if index == 0:
+            video_id = 0
+            frame_id = 0
+        else:
+            video_id = np.searchsorted(self.cumsum, index) - 1
+            frame_id = index - self.cumsum[video_id] - 1
+        frame = Image.open(self.videos[video_id][frame_id])
+        frame = self.transform(np.array(frame))
+        return frame
+
+
+    def __len__(self):
+        return len(self.videos) if self.mode == 'video' else np.sum(self.lengths)
